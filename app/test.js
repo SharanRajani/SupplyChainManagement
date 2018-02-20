0afbb27280451a279a25cd001939798492a9d7f1
AtharvaG <gomekaratharva@gmail.com>
2018-02-20 17:37:34 +0530

    Working Prototype#01

--- app/test.js
+++ app/test.js
@@ -27,14 +27,6 @@ function backtracking() {
     }
   };
   req.send();
-
-  if(xmlhttp.readyState === XMLHttpRequest.DONE && xmlhttp.status === 200) {
-    console.log(xmlhttp.responseText);
-    document.getElementById("status").innerHTML="Success!";
-  }
-  else{
-    document.getElementById("status").innerHTML="Failed!";
-  }
 }
 
 function addProduct() {
@@ -53,8 +45,9 @@ function addProduct() {
 	jsonAdd["$class"] = "xchain.logistics.Product" ;
 	jsonAdd["productId"] = Id;
 	jsonAdd["description"] = Description ;
+    jsonAdd["owner"] =  newOwner;
 	xmlhttp.send(JSON.stringify(jsonAdd));
-  jsonAdd["owner"] =  newOwner;
+
   xmlhttp.onreadystatechange = function () {
   if(xmlhttp.readyState === XMLHttpRequest.DONE && xmlhttp.status === 200) {
     console.log(xmlhttp.responseText);
@@ -85,54 +78,48 @@ function addTrader() {
 	jsonAdd["password"] = Password ;
 	jsonAdd["name"] = Name;
 	xmlhttp.send(JSON.stringify(jsonAdd));
-  if(xmlhttp.readyState === XMLHttpRequest.DONE && xmlhttp.status === 200) {
-    console.log(xmlhttp.responseText);
-    document.getElementById("status1").innerHTML="Success!";
-  }
-  else{
-    document.getElementById("status1").innerHTML="Failed!";
-  }
+    console.log(xmlhttp.readyState);
+    console.log(xmlhttp.status);
+    xmlhttp.onreadystatechange = function () {
+    if(xmlhttp.readyState === XMLHttpRequest.DONE && xmlhttp.status === 200) {
+      console.log(xmlhttp.responseText);
+      document.getElementById("status1").innerHTML="Success!";
+    }
+    else{
+      document.getElementById("status1").innerHTML="Failed!";
+    }
+  };
 }
 
 
-function transferAsset(transfer) {
-    var buyer =String(transfer.buyer);
-    var seller = transfer.seller;
-    var product = transfer.product;
-  	var password = transfer.password;
-
-    if(password  == seller.password && product.owner.password==seller.password){
-		// var a = 0;
-    	return getParticipantRegistry('xchain.logistics.Trader')
-        .then(function(participant){
-			//var buyer1=
-      		return participant.exists(buyer);
-				}).then(function(exist){
-				//	console.log(exist);
-					console.log(buyer);
-						product.owner = buyer;
-					 })
-					 .catch(function (error) {
-			    		console.log("Error");
-					}).then(function(){	return getAssetRegistry('xchain.logistics.Product')})
-
-        				.then(function (assetRegistry) {
-  							var transferNotification = getFactory().newEvent('xchain.logistics', 'TransferNotification');
-           					transferNotification.buyer = buyer;
-             				transferNotification.seller = seller;
-             				transferNotification.product = product;
-           					emit(transferNotification);
-          					console.log('Updating Registry')
-          					return assetRegistry.update(transfer.product);
-        					});
-
-	}
-else{
-throw new Error('Seller doesn’t own product.’');
-console.log('Seller doesn’t own product. ');
-}
-}
-
-function transfer(){
-
+function transfer() {
+    var sellerId = document.getElementById("sender_address").value;
+	console.log(sellerId);
+    var sellerPassword = document.getElementById("trans_password").value;
+    var productId = document.getElementById("trans_productId").value;
+    var receiverId = document.getElementById("receiver_address").value;
+    var seller = String(sellerId);
+    var password = String(sellerPassword);
+    var product = String(productId);
+    var buyer = String(receiverId);
+	console.log(productId);
+	var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance
+	xmlhttp.open("POST", "http://localhost:3000/api/xchain.logistics.Transfer");
+	xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+	var jsonAdd = {};
+	jsonAdd["$class"] ="xchain.logistics.Transfer" ;
+	jsonAdd["buyer"] = buyer;
+	jsonAdd["seller"] = seller;
+	jsonAdd["product"] = product;
+    jsonAdd["password"] = password;
+	xmlhttp.send(JSON.stringify(jsonAdd));
+    xmlhttp.onreadystatechange = function () {
+    if(xmlhttp.readyState === XMLHttpRequest.DONE && xmlhttp.status === 200) {
+      console.log(xmlhttp.responseText);
+      document.getElementById("status2").innerHTML="Success!";
+    }
+    else{
+      document.getElementById("status2").innerHTML="Failed!";
+    }
+  };
 }
